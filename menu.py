c87e1d2698e3c801ca08d1446b3775094e270817
sajad <bipelak.69@gmail.com>
2025-03-25 15:06:06 +0430

    to create menu for game

--- menu.py
+++ menu.py
@@ -2,14 +2,64 @@ import pygame
 
 pygame.init()
 
-screen_of_menu = pygame.display.set_mode((600,300))
+width , height = 800 , 500
+screen = pygame.display.set_mode((width,height))
+pygame.display.set_caption('menu')
 
+# load button images
+start_img = pygame.image.load("start_btn.png").convert_alpha()
+exit_img = pygame.image.load("exit_btn.png").convert_alpha()
+
+#button class
+class Button():
+	def __init__(self, x, y, image, scale):
+		width = image.get_width()
+		height = image.get_height()
+		self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
+		self.rect = self.image.get_rect()
+		self.rect.topleft = (x, y)
+		self.clicked = False
+
+	def draw(self, surface):
+		action = False
+		#get mouse position
+		pos = pygame.mouse.get_pos()
+
+		#check mouseover and clicked conditions
+		if self.rect.collidepoint(pos):
+			if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+				self.clicked = True
+				action = True
+
+		if pygame.mouse.get_pressed()[0] == 0:
+			self.clicked = False
+
+		#draw button on screen
+		surface.blit(self.image, (self.rect.x, self.rect.y))
+
+		return action
+
+#create button instances
+start_button = Button(100, 200, start_img, 0.8)
+exit_button = Button(450, 200, exit_img, 0.8)
+
+#game loop
 run = True
+while run:
 
-while run : 
-    for event in pygame.event.get(): 
-        if event.type == pygame.QUIT :
-            run = False
+	screen.fill((202, 228, 241))
 
-    pygame.display.update()
+	if start_button.draw(screen):
+		import main
+	if exit_button.draw(screen):
+		run = False
+
+	#event handler
+	for event in pygame.event.get():
+		#quit game
+		if event.type == pygame.QUIT:
+			run = False
+
+	pygame.display.update()
+
 pygame.quit()
